8de3a5a84e9481f9f1252ee6a80bbf40b7454cb4
Gavrilenko Egor <gavrilenkoegor18@gmail.com>
2019-05-28 13:13:20 +0300

    add ModelAdmin

--- bookspace/library/admin.py
+++ bookspace/library/admin.py
@@ -1,3 +1,34 @@
 from django.contrib import admin
+from django.contrib.auth.models import Group
+from .models import User, Book, UserAndBook
 
-# Register your models here.
+
+class BookAdmin(admin.ModelAdmin):
+    list_display = ["author", "title"]
+    list_filter = ["author",]
+    search_fields = ["author", "title"]
+
+    class Meta:
+        model = Book
+
+
+class UserAndBookAdmin(admin.ModelAdmin):
+    list_display = ["userID", "bookID", "userDescription"]
+    list_filter = ["userID"]
+
+    class Meta:
+        model = UserAndBook
+
+
+class UserAdmin(admin.ModelAdmin):
+    list_display = ["name"]
+    list_filter = ["name"]
+    search_fields = ["name"]
+
+    class Meta:
+        model = User
+
+
+admin.site.register(Book, BookAdmin)
+admin.site.register(User, UserAdmin)
+admin.site.register(UserAndBook, UserAndBookAdmin)
