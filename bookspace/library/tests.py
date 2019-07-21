3e80a4c0784c0e609c67a5c4b04a234ad5335b05
Egor <gavrilenkoegor16@gmail.com>
2019-07-21 15:07:09 +0300

    update tests

--- bookspace/library/tests.py
+++ bookspace/library/tests.py
@@ -1,4 +1,6 @@
-from django.test import TestCase, SimpleTestCase
+import datetime
+from django.test import TestCase
+from django.core.files.uploadedfile import SimpleUploadedFile
 
 from .models import User, UserAndBook, Book
 
@@ -18,52 +20,69 @@ class UserTest(TestCase):
         self.assertEquals(name, 'U')
 
     def test_get_all_user(self):
-        response = self.client.get('/')
+        response = self.client.get('/1/')
         self.assertEquals(response.status_code, 200)
         self.assertTemplateUsed(response, 'users.html')
 
     def test_add_user(self):
-        user = User.objects.get(name='U')
-        name = user.name
-        response = self.client.post('/api/addUser', {'name': name})
-        self.assertEquals(response.status_code, 400)
-
         response = self.client.post('/api/addUser', {'name': 'P'})
-        self.assertEquals(response.status_code, 200)
+        self.assertEquals(response.status_code, 302)
+
+    def test_edit_user(self):
+        image = SimpleUploadedFile(name='2.jpg', content=open('./media/user_image/2.jpg', 'rb').read(), content_type='image/*')
+        response = self.client.post('/api/editUser', {'name': 'U', 'image': image})
+        self.assertEquals(response.status_code, 302)
+
+    def test_delete_user(self):
+        response = self.client.post('/api/delete', {'name': 'U'})
+        self.assertEquals(response.status_code, 302)
+
 
 class BookTest(TestCase):
     def setUp(self):
-        book = Book.objects.create(author='A', title='T')
+        book = Book.objects.create(author='A', title='T', price=100, number_of_pages=200)
 
     def test_book(self):
-        book = Book.objects.get(author='A', title='T')
+        book = Book.objects.get(author='A', title='T', price=100, number_of_pages=200)
         author = book.author
         title = book.title
+        price = book.price
+        number_of_pages = book.number_of_pages
         self.assertEquals(author, 'A')
         self.assertEquals(title, 'T')
+        self.assertEquals(price, 100)
+        self.assertEquals(number_of_pages, 200)
 
     def test_get_book(self):
-        book = Book.get_book(author='A', title='T')
+        book = Book.get_book(author='A', title='T', price=100, number_of_pages=200)
         author = book.author
         title = book.title
+        price = book.price
+        number_of_pages = book.number_of_pages
         self.assertEquals(author, 'A')
         self.assertEquals(title, 'T')
+        self.assertEquals(price, 100)
+        self.assertEquals(number_of_pages, 200)
 
     def test_get_or_create_book(self):
-        book = Book.get_book_or_create(author='1', title='2')
+        book = Book.get_book_or_create(author='1', title='2', price=100, number_of_pages=200)
         author = book.author
         title = book.title
+        price = book.price
+        number_of_pages = book.number_of_pages
         self.assertEquals(author, '1')
         self.assertEquals(title, '2')
+        self.assertEquals(price, 100)
+        self.assertEquals(number_of_pages, 200)
 
 
 class UserAndBookTest(TestCase):
     def setUp(self):
-        book = Book.objects.create(author='A1', title='T1')
+        book = Book.objects.create(author='A1', title='T1', price=100, number_of_pages=200)
         user = User.objects.create(name='U1')
-        UserAndBook.objects.create(bookID=book, userID=user, userDescription="1")
+        UserAndBook.objects.create(bookID=book, userID=user, description="1")
 
-    def test_post_get_user_books(self):
+    def test_get_user_books(self):
         user = User.objects.get(name='U1')
         name = user.name
         request = "/api/getBooks?name="+name
@@ -71,34 +90,49 @@ class UserAndBookTest(TestCase):
         self.assertEquals(response.status_code, 200)
         self.assertTemplateUsed(response, 'books.html')
 
-    def test_post_edit_description(self):
+    def test_edit_description(self):
         user = User.objects.get(name='U1')
         name = user.name
-        book = Book.objects.get(author='A1', title='T1')
+        book = Book.objects.get(author='A1', title='T1', price=100, number_of_pages=200)
         author = book.author
         title = book.title
+        price = book.price
+        number_of_pages = book.number_of_pages
         description = '2'
         response = self.client.post('/api/editDescription',
                                     {'author': author, 'title': title,
+                                     'price': price, 'number_of_pages': number_of_pages,
                                      'name': name, 'description': description})
-        self.assertEquals(response.status_code, 200)
+        self.assertEquals(response.status_code, 302)
         userAndBook = UserAndBook.objects.get(userID=user, bookID=book)
-        self.assertEquals(userAndBook.userDescription, '2')
+        self.assertEquals(userAndBook.description, '2')
 
     def test_assign_book(self):
         user = User.objects.get(name='U1')
         name = user.name
-        book = Book.objects.get(author='A1', title='T1')
+        author = 'A2'
+        title = 'T2'
+        price = 100
+        number_of_pages = 200
+        description = '2'
+
+        response = self.client.post('/api/assignBook',
+                                    {'author': author, 'title': title,
+                                     'price': price, 'number_of_pages': number_of_pages,
+                                     'name': name, 'description': description})
+        self.assertEquals(response.status_code, 302)
+
+    def test_delete_book(self):
+        user = User.objects.get(name='U1')
+        name = user.name
+        book = Book.objects.get(author='A1', title='T1', price=100, number_of_pages=200)
         author = book.author
         title = book.title
-        description = '2'
-        response = self.client.post('/api/assignBook',
-                                    {'author': author, 'title': title,
-                                     'name': name, 'description': description})
-        self.assertEquals(response.status_code, 400)
+        price = book.price
+        number_of_pages = book.number_of_pages
 
-        title = "11"
-        response = self.client.post('/api/assignBook',
+        response = self.client.post('/api/deleteBook',
                                     {'author': author, 'title': title,
-                                     'name': name, 'description': description})
-        self.assertEquals(response.status_code, 200)
+                                     'price': price, 'number_of_pages': number_of_pages,
+                                     'name': name})
+        self.assertEquals(response.status_code, 302)
